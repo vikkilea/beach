041e43b26b42cbeac81c5359afbe4f15c9a58c8c
Victoria Shaile <7543592+vikkilea@users.noreply.github.com>
2025-12-31 17:32:58 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,52 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "player":return img`
+. . a a . . 
+. a a a a . 
+a a a a a a 
+a a a a a a 
+. a a a a . 
+. . a a . . 
+`;
+            case "sand_tile":return img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,61 @@
-
+namespace SpriteKind {
+    export const MapTile = SpriteKind.create()
+}
+function setupStatics () {
+    MAP_WIDTH = 12
+    MAP_HEIGHT = 12
+    MAP_TILE_SIZE = 17
+}
+function setupMap () {
+    s_tiles = []
+    x = 0
+    for (let index = 0; index < MAP_WIDTH; index++) {
+        s_tiles_inner = []
+        y = 0
+        for (let index = 0; index < MAP_HEIGHT; index++) {
+            tile = sprites.create(assets.image`sand_tile`, SpriteKind.MapTile)
+            tile.setPosition(MAP_TILE_SIZE * x, MAP_TILE_SIZE * y)
+            s_tiles_inner.push(tile)
+            y += 1
+            console.log(y)
+        }
+        x += 1
+        s_tiles.push(s_tiles_inner)
+        console.log(x)
+    }
+}
+function setupPlayer () {
+    s_player = sprites.create(assets.image`player`, SpriteKind.Player)
+}
+function setup () {
+    setupStatics()
+    setupMap()
+    setupPlayer()
+    gameIsSetup = TRUE
+}
+function movePlayer () {
+    controller.moveSprite(s_player)
+}
+function helpers2 () {
+    TRUE = 1
+    FALSE = 0
+}
+let FALSE = 0
+let TRUE = 0
+let gameIsSetup = 0
+let s_player: Sprite = null
+let tile: Sprite = null
+let y = 0
+let s_tiles_inner: Sprite[] = []
+let x = 0
+let s_tiles: Sprite[][] = []
+let MAP_TILE_SIZE = 0
+let MAP_HEIGHT = 0
+let MAP_WIDTH = 0
+helpers2()
+setup()
+forever(function () {
+    if (gameIsSetup == TRUE) {
+        movePlayer()
+    }
+})
