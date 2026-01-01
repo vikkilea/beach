2ec7760fd870af8cea863517966d29b9a601a78c
Victoria Shaile <7543592+vikkilea@users.noreply.github.com>
2026-01-01 18:08:41 +0100

    fix build

--- images.g.ts
+++ images.g.ts
@@ -1,10 +1,10 @@
 // Auto-generated code. Do not edit.
 namespace myImages {
 
-    helpers._registerFactory("image", function (name: string) {
-        switch (helpers.stringTrim(name)) {
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
             case "image1":
-            case "player": return img`
+            case "player":return img`
 . . a a . . 
 . a a a a . 
 a a a a a a 
@@ -12,7 +12,7 @@ a a a a a a
 . a a a a . 
 . . a a . . 
 `;
-            case "sand_tile": return img`
+            case "sand_tile":return img`
 b b b b b b b b b b b b b b b b 
 b b b b b b b b b b b b b b b b 
 b b b b b b b b b b b b b b b b 
@@ -31,26 +31,26 @@ b b b b b b b b b b b b b b b b
 b b b b b b b b b b b b b b b b 
 `;
             case "image2":
-            case "guest": return img`
-. . . . e . . . . . e . . . . . 
-. . . e e e . . . e e e . . . . 
-. . . e e e e e e e e e . . . . 
-. . . e e d 8 e 8 d e e . . . . 
-. . . . e e e e e e e . . . . . 
-. . . . e e e f e e e . . . . . 
-. . . . . e e e e e . . . . . . 
-. . . . e 8 e e e 8 e . . . . . 
-. . . e e 8 8 8 8 8 e e . . . . 
-. . . e e 8 8 8 8 8 e e . . . . 
-. . . e e 6 6 6 6 6 e e . . . . 
-. . . . e 6 6 6 6 6 e . . . . . 
-. . . . . 6 6 6 6 6 . . . . . . 
-. . . . . e e . e e . . . . . . 
-. . . . . e e . e e . . . . . . 
-. . . . . e e . e e . . . . . . 
+            case "guest":return img`
+. . . . . 3 3 b 3 3 d d 3 3 . . 
+. . . . 3 1 1 d 3 d 1 1 1 1 3 . 
+. . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
+. . 3 d d 1 1 1 d d 1 1 1 3 3 3 
+. 3 1 1 d 1 1 1 1 d d 1 1 b . . 
+. 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
+. b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
+. 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
+. 4 4 d 1 1 1 1 1 1 d d d b b . 
+. 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+4 5 5 d 5 5 d b b b d d 3 . . . 
+4 5 5 5 d d d d 4 4 b 3 . . . . 
+. 4 5 5 5 4 4 4 . . . . . . . . 
+. . 4 4 4 . . . . . . . . . . . 
 `;
             case "image3":
-            case "car": return img`
+            case "car":return img`
 . . . . . . . . . . . . 
 . . . . . . . . . . . . 
 . . . 5 6 6 6 6 5 . . . 
@@ -72,15 +72,15 @@ b b b b b b b b b b b b b b b b
         return null;
     })
 
-    helpers._registerFactory("animation", function (name: string) {
-        switch (helpers.stringTrim(name)) {
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
 
         }
         return null;
     })
 
-    helpers._registerFactory("song", function (name: string) {
-        switch (helpers.stringTrim(name)) {
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
 
         }
         return null;
