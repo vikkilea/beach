11318eaf090f0787a25ee806cdd234b032581f36
Victoria Shaile <7543592+vikkilea@users.noreply.github.com>
2026-01-01 16:21:47 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
 namespace SpriteKind {
     export const MapTile = SpriteKind.create()
     export const Guest = SpriteKind.create()
-}
-function guestManager () {
-	
+    export const Car = SpriteKind.create()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     spawnNewGuest()
 })
+function car (c: Sprite) {
+	
+}
 function helpers2 () {
     TRUE = 1
     FALSE = 0
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    spawnNewCar()
+})
 function setupStatics () {
     TILEMAP = tilemap`level1`
 }
@@ -26,37 +30,61 @@ function setupTilemap () {
 function setupPlayer () {
     s_player = sprites.create(assets.image`player`, SpriteKind.Player)
 }
+behaviors.onEnter(SpriteKind.Guest, "new", function (sprite) {
+    direction = randint(-1, 1)
+    vx = randint(2, 6) * direction
+    vy = randint(2, 6) * direction
+    sprite.setVelocity(vx, vy)
+    behaviors.setState(sprite, "moving")
+})
 function setup () {
     setupStatics()
     setupTilemap()
     setupPlayer()
     gameIsSetup = TRUE
 }
-function guest (g: Sprite) {
-    if (!(sprites.readDataBoolean(g, "moving"))) {
-        direction = randint(-1, 1)
-        vx = randint(2, 6) * direction
-        vy = randint(2, 6) * direction
-        g.setVelocity(vx, vy)
-        sprites.setDataNumber(g, "moving", randint(400, 500))
-    } else {
-        sprites.changeDataNumberBy(g, "moving", -1)
+function updateCars () {
+    for (let internal_c of cars) {
+        car(internal_c)
     }
+}
+behaviors.onEnter(SpriteKind.Guest, "moving", function (sprite) {
+    sprites.setDataNumber(sprite, "moving", randint(400, 500))
+})
+behaviors.onUpdate(SpriteKind.Guest, "moving", function (sprite) {
+    sprites.changeDataNumberBy(sprite, "moving", -1)
+    if (sprites.readDataNumber(sprite, "") <= 0) {
+        behaviors.setState(sprite, "idle")
+    }
+})
+function guest (g: Sprite) {
+	
 }
 function movePlayer () {
     controller.moveSprite(s_player)
     scene.cameraFollowSprite(s_player)
 }
+behaviors.onEnter(SpriteKind.Car, "new", function (sprite) {
+	
+})
 function spawnNewGuest () {
     new_guest = sprites.create(assets.image`guest`, SpriteKind.Guest)
+    behaviors.setState(new_guest, "new")
     sprites.setDataNumber(new_guest, "moving", 0)
     guests.push(new_guest)
 }
+function spawnNewCar () {
+    new_car = sprites.create(assets.image`car`, SpriteKind.Car)
+    behaviors.setState(new_car, "new")
+    cars.push(new_car)
+}
+let new_car: Sprite = null
 let new_guest: Sprite = null
+let cars: Sprite[] = []
+let gameIsSetup = 0
 let vy = 0
 let vx = 0
 let direction = 0
-let gameIsSetup = 0
 let s_player: Sprite = null
 let guests: Sprite[] = []
 let TILEMAP: tiles.TileMapData = null
@@ -70,6 +98,8 @@ forever(function () {
     }
 })
 forever(function () {
-    guestManager()
     updateGuests()
+})
+forever(function () {
+    updateCars()
 })
